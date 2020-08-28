43ef858dba042514063a070830e61a4f340a451c
Matthew Heyner <mheyner@gmail.com>
2020-08-28 16:36:40 -0400

    l

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 
 const app = express()
-const PORT = 8000
+var port = process.env.PORT || 8080
 app.get('/', (req, res) => res.send('Express + TypeScript Server'))
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
 })
