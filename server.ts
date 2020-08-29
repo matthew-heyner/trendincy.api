4d155838c9a3cc44fbdd01fda1c06c026790fdbc
Matthew Heyner <mheyner@gmail.com>
2020-08-29 16:22:07 -0400

    Adding GetTrendPlaces

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import express from 'express'
+import { GetTrendPlaces } from './services/TwitterAuthenticator'
 
 const app = express()
 var port = process.env.PORT || 8080
-app.get('/', (req, res) => res.send('Express + TypeScript Server'))
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+app.get('/', (req, res) => res.send(GetTrendPlaces))
+app.listen(port)
--- services/TwitterAuthenticator.ts
+++ services/TwitterAuthenticator.ts
@@ -1,21 +1,22 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios'
 
-const config: AxiosRequestConfig = {
+const trendPlaces: AxiosRequestConfig = {
     method: 'post',
-    url: 'https://api.twitter.com/oauth2/token',
+    url: 'https://api.twitter.com/1.1/trends/place.json',
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Authorization': 'OAuth oauth_consumer_key="AhILi0Y7BWNodujUVS6hWLYAB",oauth_token="581278002-bvqliK63SbBGfkK8oRPFQojmEG8QTfojiEj793MM",oauth_signature_method="HMAC-SHA256",oauth_timestamp="1594252167",oauth_nonce="GhxzmqrMnct",oauth_version="1.0",oauth_signature="RN3T0Wy5OOtcLIh9Vw5jJJ22hwoYCPWMdVWXws3zerY%3D"',
-        'Cookie': 'personalization_id="v1_IYO+tbBobLVHro2nCmYnJw=="; guest_id=v1%3A159330059161152026'
-    }
-};
+        Authorization:
+            'OAuth oauth_consumer_key="AhILi0Y7BWNodujUVS6hWLYAB",oauth_token="581278002-zivYuX1ssiL9Kp3ODx0hAMJKN0NRbrWkyp9rKaQF",oauth_signature_method="HMAC-SHA256",oauth_timestamp="1598732119",oauth_nonce="WAS2wgAtin8",oauth_version="1.0",oauth_signature="%2BBANWvn4ul2ADIte7RJPjZ0v2lwJbGR4OQ%2F5ekYy2E0%3D"',
+        Cookie:
+            'personalization_id="v1_IYO+tbBobLVHro2nCmYnJw=="; guest_id=v1%3A159330059161152026',
+    },
+}
 
-export const TwitterApi = () => {
-    axios(config)
+export const GetTrendPlaces = () => {
+    axios(trendPlaces)
         .then(function (response) {
-            return JSON.stringify(response.data);
+            return JSON.stringify(response.data)
         })
         .catch(function (error) {
-            throw new Error(error);
-        });
+            throw new Error(error)
+        })
 }
