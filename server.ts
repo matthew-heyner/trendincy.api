6722ea2bfef3cbca342e501951789db38a847975
Matthew Heyner <mheyner@gmail.com>
2020-08-30 16:58:16 -0400

    Testing api on heroku,

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
-import { GetTrendPlaces } from './services/TwitterAuthenticator'
+import { GetTrendPlaces, testing } from './services/TwitterAuthenticator'
 
 const app = express()
 var port = process.env.PORT || 8080
-app.get('/', (req, res) => res.send(GetTrendPlaces))
+
+app.get('/', (req, res) => {
+    console.log('testing GetTrendPlaces')
+    console.log(GetTrendPlaces())
+    //    var jsonRespons = GetTrendPlaces()
+    //    console.log(res.json(jsonRespons))
+})
 app.listen(port)
--- services/TwitterAuthenticator.ts
+++ services/TwitterAuthenticator.ts
@@ -5,18 +5,24 @@ const trendPlaces: AxiosRequestConfig = {
     url: 'https://api.twitter.com/1.1/trends/place.json',
     headers: {
         Authorization:
-            'OAuth oauth_consumer_key="AhILi0Y7BWNodujUVS6hWLYAB",oauth_token="581278002-zivYuX1ssiL9Kp3ODx0hAMJKN0NRbrWkyp9rKaQF",oauth_signature_method="HMAC-SHA256",oauth_timestamp="1598732119",oauth_nonce="WAS2wgAtin8",oauth_version="1.0",oauth_signature="%2BBANWvn4ul2ADIte7RJPjZ0v2lwJbGR4OQ%2F5ekYy2E0%3D"',
+            'OAuth oauth_consumer_key="dYiQ9FqI2Xxa9DIWQwrce1VxZ",oauth_token="581278002-Dh6CGNHvOgmHbnbv7IZgcyGG03qGcZRAWDdSNx71",oauth_signature_method="HMAC-SHA256",oauth_timestamp="1598820803",oauth_nonce="QQbtT95xBa7",oauth_version="1.0",oauth_signature="iOeWlVZwpWgc5U7ES11LE9%2BcQRMpEFXkuC0i6BvH5rI%3D"',
         Cookie:
             'personalization_id="v1_IYO+tbBobLVHro2nCmYnJw=="; guest_id=v1%3A159330059161152026',
     },
 }
 
+export const testing = () => {
+    axios.get('http://webcode.me').then((resp) => {
+        console.log(resp.data)
+    })
+}
+
 export const GetTrendPlaces = () => {
     axios(trendPlaces)
-        .then(function (response) {
+        .then((response) => {
             return JSON.stringify(response.data)
         })
-        .catch(function (error) {
-            throw new Error(error)
+        .catch((error) => {
+            console.log('The error is: ' + error)
         })
 }
