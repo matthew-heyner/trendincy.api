5db1087fe2320ad309548b59aaf9f8ffb9e0e182
Matthew Heyner <mheyner@gmail.com>
2021-09-12 18:20:47 -0400

    remove debug code

--- src/controllers/country-trends.js
+++ src/controllers/country-trends.js
@@ -1,10 +1,6 @@
 const OAuthConnectorService = require('../services/oauthConnectorService');
 
 function CountryController(url, callback){
-    console.log('CONSUMER_KEY: ' + process.env.CONSUMER_KEY);
-    console.log('CONSUMER_SECRET: ' + process.env.CONSUMER_SECRET);
-    console.log('ACCESS_TOKEN: ' + process.env.ACCESS_TOKEN);
-    console.log('ACCESS_TOKEN_SECRET: ' + process.env.ACCESS_TOKEN_SECRET);
     OAuthConnectorService.oauthConnector.get(url, 
         OAuthConnectorService.ACCESS_TOKEN, 
         OAuthConnectorService.ACCESS_TOKEN_SECRET, 
